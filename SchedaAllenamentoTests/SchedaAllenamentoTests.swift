eacf985764510b7264c3858315f03e8deb81cdbd
Ludovico Nigro <157514895+ludoviconigro@users.noreply.github.com>
2025-04-02 16:52:39 +0200

    Add files via upload

---
+++ SchedaAllenamentoTests/SchedaAllenamentoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SchedaAllenamentoTests.swift
+//  SchedaAllenamentoTests
+//
+//  Created by Ludovico Nigro on 31/03/25.
+//
+
+import Testing
+@testable import SchedaAllenamento
+
+struct SchedaAllenamentoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
